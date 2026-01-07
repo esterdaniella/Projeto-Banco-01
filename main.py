4e4e6e82acbdbfdcedc74a75e7d03f73a55bb48d
Ester Daniella Carvalho dos Santos <ester0808daniella@gmail.com>
2026-01-07 20:09:59 -0300

    Removendo arquivos desnecessários do cache

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
+
 from conta import Conta
 
-# Criando a conta inicial
 conta_dani = Conta(numero="123-4", titular="Dani", senha="123", saldo_inicial=500)
 
 print("=-" * 25)
 print(f"Bem-Vindo {conta_dani.titular}! Esse é o terminal do Banco Carvalho")
 print("=-" * 25)
 
-while True: 
+while True:   
+
     print("\n[1] Sacar")
     print("[2] Depositar")
     print("[3] Sair")
     print("[4] Extrato")
 
-    opcao = input('Escolha uma opção: ')
+    opcao = input('\nEscolha uma opção: ')
 
     if opcao == "1": 
         try: 
@@ -38,3 +39,7 @@ while True:
 
     else: 
         print('Opção inválida. Escolha entre 1 e 4.')
+
+    # 2. A PAUSA: Se o usuário não escolheu sair (3), o programa espera um Enter
+    if opcao != "3":
+        input('\n--- Pressione ENTER para voltar ao menu ---')
